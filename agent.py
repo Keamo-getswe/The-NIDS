33a703d9904aa0e3046173c547c2e60bc1ee2187
Keamo-getswe <morobikmobi@gmail.com>
2024-07-27 16:31:52 +0200

    Clean up agent to use training data and production data
    - Remove call to super class since QObject is removed as parent.
    - Add default parameter to set_data_from_files method. Also move
      filereader to this method.
    - Change preprocess method name to training_preprocess in order to
      preprocess training data. Also implement the code previously in
      Preprocessor.preprocess since this preprocessing will differ from
      production data preprocessing.
    - Add production_preprocess method to handle production data.

--- agent.py
+++ agent.py
@@ -1,16 +1,46 @@
 from filereader import FileReader
+from sklearn.model_selection import train_test_split
 from preprocessor import Preprocessor
+import pandas as pd
+import utility
+import sys
 
-class Agent:
-    def __init__(self, path):
-        self.__reader = FileReader(path)
+class Agent():
+    def __init__(self):
         self.__preprocessor = Preprocessor()
 
-    def set_data_from_files(self):
-        return self.__reader.read_files()
+    def set_data_from_files(self, path="C:\\Users\\morob\\Documents\\Work\\Honours Project\\The-NIDS\\CICIDS2017\\MachineLearningCVE"):
+        fileReader = FileReader(path)
+        return fileReader.read_files()
 
-    def preprocess(self):
+    def training_preprocess(self):
         data = self.set_data_from_files()
-        train_data, train_labels, test_data, test_labels = self.__preprocessor.process_data(data)
 
-        return train_data, train_labels, test_data, test_labels
+        processed_data = self.__preprocessor.handle_null_values(data)
+        #processed_data = self.__preprocessor.extract_features(processed_data)
+
+        #Train/test split data
+        processed_train_data, processed_test_data = train_test_split(processed_data, test_size=utility.TEST_SIZE,
+                                                     random_state=utility.RANDOM_STATE_SEED)
+        
+        processed_train_data, processed_train_labels = self.__preprocessor.split_data(processed_train_data)
+        processed_test_data, processed_test_labels = self.__preprocessor.split_data(processed_test_data)
+        
+        #Normalise data
+        processed_train_data = self.__preprocessor.normalise_values(processed_train_data)
+        processed_test_data = self.__preprocessor.normalise_values(processed_test_data)
+
+        #Enumerate labels
+        processed_train_labels = self.__preprocessor.enumerate_labels(processed_train_labels)
+        processed_test_labels = self.__preprocessor.enumerate_labels(processed_test_labels)
+
+        return processed_train_data.iloc[:, :utility.INPUT_DIMENSION], processed_train_labels, processed_test_data.iloc[:, :utility.INPUT_DIMENSION], processed_test_labels
+    
+    def production_preprocess(self, data):
+        data = [float(x) for x in data.split(",")[:-1]]
+        df = pd.DataFrame(data)
+        df = df.T
+        df = self.__preprocessor.handle_null_values(df)
+        df = self.__preprocessor.normalise_values(df)
+        return df.iloc[:,:utility.INPUT_DIMENSION]
+        
