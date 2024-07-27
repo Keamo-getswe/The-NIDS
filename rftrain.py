730146e1d64b5a2a97bf15173b97b4b2929ce2bb
Keamo-getswe <morobikmobi@gmail.com>
2024-07-27 16:43:52 +0200

    Clean up issues due to other changes.
    - Remove Agent since test data was saved to binary.
    - Add constants to autoencoder constructor to fix magic number issue and
      make future adjustments easier.
    - Remove variables taking output from autoencoder encode() method.
      Retrieved encoded data from getter instead.

--- rftrain.py
+++ rftrain.py
@@ -1,16 +1,19 @@
 from randomforest import RandomForest
 from autoencoder import AutoEncoder
-from agent import Agent
 import joblib
 import utility
+import os
 
 def train_and_save_model():
     rf = RandomForest()
-    ae = AutoEncoder(5, 3)
+    input_size = utility.INPUT_DIMENSION
+    hidden_size = utility.HIDDEN_DIMENSION
+    ae = AutoEncoder(input_size, hidden_size)
     ae.load_model()
     data = joblib.load(utility.TRAIN_TEST_DATA_PATH)
     train_data = data["train_data"]
-    _, reduced_features = ae.encode(train_data)
+    ae.encode(train_data)
+    reduced_features = ae.get_encoded_data()
     train_labels = data["train_labels"]
     rf.train(reduced_features, train_labels)
     rf.save_model()
