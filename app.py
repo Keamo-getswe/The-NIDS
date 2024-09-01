69f6dc9e6e2ab0bd21c63fcefe226eadfb4b0443
Keamo-getswe <morobikmobi@gmail.com>
2024-09-01 17:30:09 +0200

    Update to reflect changes elsewhere and combine calls to individual models to call pipelines
    * Add hidden dimensions to array for autoencoder constructor parameter
      precondition.
    * Rename API call to pipeline 1 and combine autoencoder and random
      forest call into it.

--- app.py
+++ app.py
@@ -3,38 +3,33 @@ import pandas as pd
 from autoencoder import AutoEncoder
 from randomforest import RandomForest
 import  utility
+import cupy as cp
 import ast
 import sys
 
 app = Flask(__name__)
 
 # Load models
-ae = AutoEncoder(utility.INPUT_DIMENSION, utility.HIDDEN_DIMENSION)
+hidden_dimensions = [utility.HIDDEN_DIMENSION, utility.BOTTLENECK_DIMENSION]
+ae = AutoEncoder(utility.INPUT_DIMENSION, hidden_dimensions)
 ae.load_model()
 
 rf = RandomForest()
 rf.load_model()
 
-@app.route('/autoencoder/predict', methods=['POST'])
+@app.route('/pipelineA/predict', methods=['POST'])
 def ae_predict():
     json_data = request.json
     #Format data to handle request
     data_list = ast.literal_eval(json_data)
     data_list = list(data_list[0].values())
     data_point = pd.DataFrame(data_list)
-    ae.encode(data_point.T)
-    p = ae.get_encoded_data()
-    return jsonify(p.tolist())
-
-@app.route('/randomforest/predict', methods=['POST'])
-def rf_predict():
-    json_data = request.json
-    #Format data to handle request
-    data_list = ast.literal_eval(json_data)
-    data_point = pd.DataFrame(data_list)
-
-    p = rf.predict(data_point.T)
-    return jsonify(p.tolist())
+    gpu_data_point = cp.array(data_point)
+    ae.encode(gpu_data_point.T)
+    latent_space = ae.get_encoded_data()
+    latent_space = pd.DataFrame(cp.asnumpy(latent_space))
+    prediction = rf.predict(latent_space)
+    return jsonify(prediction.tolist())
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000)
