d64d2d87f939df2c707fd696ff411d007d3de8e8
Keamo-getswe <morobikmobi@gmail.com>
2024-09-01 16:34:25 +0200

    Update to reflect changes made elsewhere
    * Import cupy to covert autoencoder cupy to numpy for sklearn
      randomforest.
    * Add hidden layers to array to match autoencoder hidden size
      constructor paramater precondition.

--- rftrain.py
+++ rftrain.py
@@ -1,5 +1,6 @@
 from randomforest import RandomForest
 from autoencoder import AutoEncoder
+import cupy as cp
 import joblib
 import utility
 import os
@@ -7,14 +8,15 @@ import os
 def train_and_save_model():
     rf = RandomForest()
     input_size = utility.INPUT_DIMENSION
-    hidden_size = utility.HIDDEN_DIMENSION
+    hidden_size = [utility.HIDDEN_DIMENSION, utility.BOTTLENECK_DIMENSION]
     ae = AutoEncoder(input_size, hidden_size)
     ae.load_model()
     data = joblib.load(utility.TRAIN_TEST_DATA_PATH)
     train_data = data["train_data"]
-    ae.encode(train_data)
+    ae.encode(cp.array(train_data.to_numpy()))
     reduced_features = ae.get_encoded_data()
     train_labels = data["train_labels"]
+    reduced_features = cp.asnumpy(reduced_features)
     rf.train(reduced_features, train_labels)
     rf.save_model()
 
