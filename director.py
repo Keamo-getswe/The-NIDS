d64d3a8d789274d837acda76861b16978febb086
Keamo-getswe <morobikmobi@gmail.com>
2024-09-01 17:43:27 +0200

    Remove code referencing 2 models in pipelineA and refer to the pipeline in calls.
    Note that this will need to be updated to allow for second pipeline
    to be called as well.

--- director.py
+++ director.py
@@ -5,29 +5,17 @@ import sys
 
 class Director:
     def __init__(self):
-        self.__ae_url = 'http://localhost:5000/autoencoder/predict'
-        self.__rf_url = 'http://localhost:5000/randomforest/predict'
+        self.__pipelineA_url = 'http://localhost:5000/pipelineA/predict'
 
     @QtCore.Slot()
     def run_pipeline(self, data):
-        latent_repr = self.__request_ae_latent_repr(data)
-        latent_repr = latent_repr[0]
-        prediction = self.__request_rf_predict(latent_repr)
+        prediction = self.__request_pipelineA(data)
         return prediction
 
-    def __request_ae_latent_repr(self, data):
+    def __request_pipelineA(self, data):
         data_json = data.to_json(orient='records') 
         headers = {'Content-Type': 'application/json'}
-        response = requests.post(self.__ae_url, json=data_json, headers=headers)
-
-        if response.status_code == 200:
-            return response.json()
-    
-    def __request_rf_predict(self, data):
-        temp = pd.DataFrame(data)
-        data_json = temp.to_json(orient='records')
-        headers = {'Content-Type': 'application/json'}
-        response = requests.post(self.__rf_url, json=data_json, headers=headers)
+        response = requests.post(self.__pipelineA_url, json=data_json, headers=headers)
 
         if response.status_code == 200:
             return response.json()
